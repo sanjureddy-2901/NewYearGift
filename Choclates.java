35a6f60d5cc1f4a0a798e485928d441e754af650
sanjureddy-2901 <60321263+sanjureddy-2901@users.noreply.github.com>
2020-02-05 22:52:50 +0530

    Create Choclates.java

---
+++ Choclates.java
@@ -0,0 +1,8 @@
+package com.belhard.practice.candies;
+public class Choclate extends Production
+{
+public Choclate(String nmae,String type,double cost,int weight)
+{
+super(name,type,cost,weight);
+}
+}

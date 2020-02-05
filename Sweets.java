793b03d98e26c6953720ac121f75680390e9f900
sanjureddy-2901 <60321263+sanjureddy-2901@users.noreply.github.com>
2020-02-05 22:58:20 +0530

    Create Sweets.java

---
+++ Sweets.java
@@ -0,0 +1,9 @@
+package com.belhard.practise.candies;
+
+public class Sweets extends Production
+{
+public Sweets(String name,String type,double cost,int weight)
+{
+super(name,type,cost,weight);
+}
+}
